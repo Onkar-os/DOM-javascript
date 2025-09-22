0db8c7c23069559ae26342ad060a3cd184b44f2f
git-demo <shindeonkar735@gmail.com>
2025-09-22 19:16:44 +0530

    add new file

---
+++ DOMEvent.js
@@ -0,0 +1,30 @@
+var a
+var b
+var c 
+function add(){
+a=parseInt(f1.fno.value)
+b=parseInt(f1.sno.value)
+c=a+b
+f1.result.value=c
+}
+
+function sub(){
+a=parseInt(f1.fno.value)
+b=parseInt(f1.sno.value)
+c=a-b
+f1.result.value=c
+}
+
+function mul(){
+a=parseInt(f1.fno.value)
+b=parseInt(f1.sno.value)
+c=a*b
+f1.result.value=c
+}
+
+function div(){
+a=parseInt(f1.fno.value)
+b=parseInt(f1.sno.value)
+c=a/b
+f1.result.value=c
+}
