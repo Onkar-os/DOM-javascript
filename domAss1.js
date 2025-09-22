177286f3e7293623d6729e2c7db5635799e54e08
git-demo <shindeonkar735@gmail.com>
2025-09-22 19:14:03 +0530

    add new js file

---
+++ domAss1.js
@@ -0,0 +1,18 @@
+var a 
+var b 
+var c 
+var u
+function show(){
+a= f1.user.value
+b= f1.email.value
+c= f1.password.value
+
+u=f1.user.value
+document.getElementById("username").innerHTML=u
+u=f1.email.value
+document.getElementById("email").innerHTML=u
+
+
+u=f1.password.value
+document.getElementById("password").innerHTML=u
+}
